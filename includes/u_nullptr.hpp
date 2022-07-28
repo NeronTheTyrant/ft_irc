e47a598ec9b0ca0ad255d2e1360f6be8ad8a43d5
LeoBourret <bourretleo@hotmail.fr>
2022-07-28 18:42:14 +0000

    added some utilitary stuff like our own enable_if u_nullptr and itos (int to string)

---
+++ includes/u_nullptr.hpp
@@ -0,0 +1,19 @@
+#ifndef U_NULLPTR_HPP
+# define U_NULLPTR_HPP
+
+static class u_nullptr_t
+{
+    public:
+               template<class T>
+        operator T*() const { return (0); }
+
+               template<class C, class T>
+        operator T C::*() const { return (0); }
+
+    private:
+
+               void operator&() const;
+
+} u_nullptr = {};
+
+#endif
