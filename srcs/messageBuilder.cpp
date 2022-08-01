57dfdf6a89777e2dfff37e1679d5b19a850d13c3
LeoBourret <bourretleo@hotmail.fr>
2022-08-01 14:07:54 +0000

    message builder almost done, some compiler issue to solve

--- srcs/messageBuilder.cpp
+++ srcs/messageBuilder.cpp
@@ -8,6 +8,7 @@ std::string		commandMessageBuilder(const	short code, std::map<std::string, User 
 		std::string const arg1, std::string const arg2, std::string const arg3, std::string const arg4) {
 	std::string		ret;
 	std::string		strCode;
+	strCode = ft_itos(code);
 	if (code < 10)
 		strCode = std::string(2, '0').append(ft_itos(code));
 	switch (code)
