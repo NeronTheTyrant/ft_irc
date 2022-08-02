16db635025e585de91c657281f356accb264e0d7
LeoBourret <bourretleo@hotmail.fr>
2022-08-02 17:14:21 +0000

    added commandList file where we can list all the command we think are needed, added a map<string, command function> into the IRCServer class, and the map is set during the default constructor

--- srcs/IRC/IRCServer.cpp
+++ srcs/IRC/IRCServer.cpp
@@ -1,7 +1,27 @@
 #include "IRCServer.hpp"
 
 IRCServer::IRCServer(int16_t port) :
-	_epollHandler(port) {};
+	_epollHandler(port) {
+	_commands["PASS"]		= &IRCServer::pass;
+	_commands["NICK"]		= &IRCServer::nick;
+	_commands["USER"]		= &IRCServer::user;
+	_commands["QUIT"]		= &IRCServer::quit;
+	_commands["PRIVMSG"]	= &IRCServer::privmsg;
+	_commands["NOTICE"]		= &IRCServer::notice;
+	_commands["JOIN"]		= &IRCServer::join;
+	_commands["LIST"]		= &IRCServer::list;
+	_commands["NAMES"]		= &IRCServer::names;
+	_commands["MODE"]		= &IRCServer::mode;
+	_commands["OPER"]		= &IRCServer::oper;
+	_commands["TOPIC"]		= &IRCServer::topic;
+	_commands["MOTD"]		= &IRCServer::motd;
+	_commands["PING"]		= &IRCServer::ping;
+	_commands["PONG"]		= &IRCServer::pong;
+	_commands["AWAY"]		= &IRCServer::away;
+	_commands["DIE"]		= &IRCServer::die;
+	_commands["KILL"]		= &IRCServer::kill;
+	_commands["KICK"]		= &IRCServer::kick;
+};
 
 IRCServer::~IRCServer() {
 	if (_eventListener)
--- srcs/IRC/IRCServer.hpp
+++ srcs/IRC/IRCServer.hpp
@@ -4,10 +4,15 @@
 #include "Network.hpp"
 #include "EpollHandler.hpp"
 #include "IRCEventListener.hpp"
+#include <map>
+#include <vector>
 
 class IRCEventListener;
 
 class IRCServer {
+
+	typedef	std::map<std::string, void	(IRCServer::*) (User &, std::vector<std::string>)>	mapCommand;
+
 public:
 	IRCServer(int16_t port);
 	~IRCServer();
@@ -17,11 +22,36 @@ public:
 	Network &	network();
 	EpollHandler &	epollHandler();
 
+	/**
+	 * COMMAND FUNCTION
+	*/
+	void	pass(User & user, std::vector<std::string> params);
+	void	nick(User & user, std::vector<std::string> params);
+	void	user(User & user, std::vector<std::string> params);
+	void	quit(User & user, std::vector<std::string> params);
+	void	privmsg(User & user, std::vector<std::string> params);
+	void	notice(User & user, std::vector<std::string> params);
+	void	join(User & user, std::vector<std::string> params);
+	void	list(User & user, std::vector<std::string> params);
+	void	names(User & user, std::vector<std::string> params);
+	void	mode(User & user, std::vector<std::string> params);
+	void	oper(User & user, std::vector<std::string> params);
+	void	topic(User & user, std::vector<std::string> params);
+	void	motd(User & user, std::vector<std::string> params);
+	void	ping(User & user, std::vector<std::string> params);
+	void	pong(User & user, std::vector<std::string> params);
+	void	away(User & user, std::vector<std::string> params);
+	void	die(User & user, std::vector<std::string> params);
+	void	kill(User & user, std::vector<std::string> params);
+	void	kick(User & user, std::vector<std::string> params);
+
 private:
 	std::string			_name;
 	Network				_network;
 	EpollHandler		_epollHandler;
 	IRCEventListener *	_eventListener;
+	mapCommand			_commands;
+
 };
 
 #endif
