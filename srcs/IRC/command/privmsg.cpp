25692225762db8c140a4387ad06c518ba75bd53e
LeoBourret <bourretleo@hotmail.fr>
2022-08-05 17:38:41 +0000

    privmsg seems good, but i'm sure i actually only have done 20% of it, needs a checkout might of forgot stuff

--- srcs/IRC/command/privmsg.cpp
+++ srcs/IRC/command/privmsg.cpp
@@ -1,36 +1,32 @@
 #include "utils.hpp"
 #include "Network.hpp"
 
-void	privmsgToChannel(User *user, vector<std::string> params, IRCServer const &serv)
-{
+void	privmsgToChannel(User *user, vector<std::string> params, IRCServer const &serv) {
 	Channel channel = serv.network().getChannelByName(params[1]);
 
-	for (channel::Users::iterator it; it != channel._users.end(); ++it)
-	{
+	for (channel::Users::iterator it; it != channel._users.end(); ++it) {
 		if (it->first.nickname != user->nickname)
 			it->first.send(serverMessageBuilder(serv, paramsToString(params)));
 	}
 }
 
-void	privmsgCommand(User *user, vector<std::string> params, IRCServer const &serv)
-{
+void	privmsgCommand(User *user, vector<std::string> params, IRCServer const &serv) {
 	User *target;
 
-	if (params.size < 3)
-	{
+	if (params.size < 3) {
 		user.send(serverMessageBuilder(serv, commandMessageBuilder(CODE_ERR_NEEDMOREPARAMS, "PRIMSG")));
 		return ;
 	}
-	if ((serv.network().getChannelByName(params[1])) != u_nullptr)
-	{
+	if ((serv.network().getChannelByName(params[1])) != u_nullptr) {
 		privmsgToChannel(user, params, serv);
 		return ;
 	}
 	target = serv.network().getUserByName(params[1])))
-	if (target != u_nullptr)
-	{
+	if (target != u_nullptr) {
 		target.send(serverMessageBuilder(serv, paramsToString(params)));
 		if (target.isModeSet('a'))
 			user.send(serv, commandMessageBuilder(CODE_RPL_AWAY, target->getAwayMessage()));
 	}
+	else
+		user.send(serv, commandMessageBuilder(CODE_ERR_NOSUCHNICK, params[1]);
 }
