2209b2ddd3d1535bc377e5cd4cd51f6abf745284
LeoBourret <bourretleo@hotmail.fr>
2022-08-11 07:49:18 +0000

    AGAIN minor fixes on privmsg and join

--- srcs/IRC/command/join.cpp
+++ srcs/IRC/command/join.cpp
@@ -8,10 +8,10 @@ void	IRCServer::join(User *sender, vector<std::string> params) {
 	Channel *target;
 
 	if (params.size < 2) {
-		sender.send(serverMessageBuilder(serv, commandMessageBuilder(CODE_ERR_NEEDMOREPARAMS, "JOIN"));
+		sender.send(serverMessageBuilder(*this, commandMessageBuilder(CODE_ERR_NEEDMOREPARAMS, "JOIN"));
 		return ;
 	}
-	target = serv.network().getChannelByName(params[1]);
+	target = this->network().getChannelByName(params[1]);
 	if (target != u_nullptr)
 		target.addUser(sender);
 	else {
---
+++ srcs/IRC/command/part.cpp
@@ -0,0 +1,2 @@
+#include "IRCServer.hpp"
+
