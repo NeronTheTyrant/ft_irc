58a5c56ec3dd947b5de0d3431eb2413ddaa7fd35
LeoBourret <bourretleo@hotmail.fr>
2022-08-11 14:57:01 +0000

    lots of fucked fixes in mostly all my goddamn command <3

--- srcs/IRC/command/away.cpp
+++ srcs/IRC/command/away.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 
 void	IRCServer::away(User * sender, std::vector<std::string> params) {
-	if (params.size == 1 && sender->isModeSet('a') {
-		sender.send(serverMessageBuilder(*this, commandMessageBuilder(CODE_RPL_UNAWAY, sender)));
+	if (params.size() == 0 && sender->isModeSet('a')) {
+		sender->send(serverMessageBuilder(*this, commandMessageBuilder(CODE_RPL_UNAWAY, sender)));
 		return ;
 	}
-	sender.send(serverMessageBuilder(*this, commandMessageBuilder(CODE_RPL_NOWAWAY, sender));
+	sender->send(serverMessageBuilder(*this, commandMessageBuilder(CODE_RPL_NOWAWAY, sender)));
 }
--- srcs/IRC/command/join.cpp
+++ srcs/IRC/command/join.cpp
@@ -4,21 +4,21 @@
 #include <vector>
 #include <iostream>
 
-void	IRCServer::join(User *sender, vector<std::string> params) {
+void	IRCServer::join(User *sender, std::vector<std::string> params) {
 	Channel *target;
 
-	if (params.size < 2) {
-		sender.send(serverMessageBuilder(*this, commandMessageBuilder(CODE_ERR_NEEDMOREPARAMS, sender, "JOIN"));
+	if (params.size() < 1) {
+		sender->send(serverMessageBuilder(*this, commandMessageBuilder(CODE_ERR_NEEDMOREPARAMS, sender, "JOIN")));
 		return ;
 	}
-	target = this->network().getChannelByName(params[1]);
+	target = this->network().getChannelByName(params[0]);
 	if (target != u_nullptr)
-		target.addUser(sender);
+		target->addUser(sender);
 	else {
-		target = new Channel(params[1], sender);
+		target = new Channel(params[0], sender);
 		this->network().add(target);
 	}
-	target->send(serverMessageBuilder(sender, paramsToString(params));
-	sender->send(serverMessageBuilder(*this, commandMessageBuilder(CODE_RPL_NAMREPLY, sender, sender->nickname(), target->name(), target->userNickList()));
-	sender->send(serverMessageBuilder(*this, commandMessageBuilder(CODE_RPL_ENDOFNAMES, sender, sender->nickname(), target->name()));
+	target->send(serverMessageBuilder(*sender, paramsToString(params)));
+	sender->send(serverMessageBuilder(*this, commandMessageBuilder(CODE_RPL_NAMREPLY, sender, sender->nickname(), target->name(), target->userNickList())));
+	sender->send(serverMessageBuilder(*this, commandMessageBuilder(CODE_RPL_ENDOFNAMES, sender, sender->nickname(), target->name())));
 }
